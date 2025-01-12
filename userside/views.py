343b7850348da1343cf53fda62caa032bf52c24b
TrushalPatel0 <business.trushalpatel@gmail.com>
2025-01-12 15:15:56 +0530

    changesd one

--- userside/views.py
+++ userside/views.py
@@ -329,28 +329,49 @@ def trading_view_signal_webhook_listener(request):
         
             # take stop_loss limit order
             action_oco = action['action']
-            data = get_position(data['access_token'])
-            if data[0]['netPos'] > 0:
-                positions = get_position(data['access_token'])
-                liquidated_position = liquidate_position(data['access_token'], positions[0]["accountId"], positions[0]["contractId"], False)
-            
-            if action_oco == 'Buy':
-                response_market = place_order(data['access_token'], data['account_spec'],data['account_id'], "Buy", symbol, data['order_qty'], "Market", True)
-                response_oco = place_oco_order(URL, data['account_spec'],data['account_id'], data['access_token'], symbol, "Sell", data['order_qty'],  float(trading_signal['slLine']), float(trading_signal['slLine']))
-            else:
-                response_market = place_order(data['access_token'], data['account_spec'],data['account_id'], "Sell", symbol, data['order_qty'], "Market", True)
-                response_oco = place_oco_order(URL, data['account_spec'],data['account_id'], data['access_token'], symbol, "Buy", data['order_qty'],  float(sl), float(tp1))
+            Position_Data= get_position(Position_Data['access_token'])
+            response_sl_list = []
+            if order_type == "stop_loss_limit_order":
+                if Position_Data[0]['netPos'] > 0:
+                    liquidated_position = liquidate_position(data['access_token'], Position_Data[0]["accountId"], Position_Data[0]["contractId"], False)
+                
+                if action_oco == 'Buy':
+                    response_market = place_order(data['access_token'], data['account_spec'],data['account_id'], "Buy", symbol, data['order_qty'], "Market", True)
+                    response_oco = place_oco_order(URL, data['account_spec'],data['account_id'], data['access_token'], symbol, "Sell", data['order_qty'],  float(trading_signal['slLine']), float(trading_signal['tp1Line']))
+                else:
+                    response_market = place_order(data['access_token'], data['account_spec'],data['account_id'], "Sell", symbol, data['order_qty'], "Market", True)
+                    response_oco = place_oco_order(URL, data['account_spec'],data['account_id'], data['access_token'], symbol, "Buy", data['order_qty'],  float(trading_signal['slLine']), float(trading_signal['tp1Line']))
 
+            elif order_type == "multiple_take_profit":
+                if action_oco == 'Buy':
+                    response_entry = place_order(data['access_token'], data['account_spec'],data['account_id'], "Buy", symbol, 3, "Market", True)  # order qty = 3
+                    response_tp1 = place_order(data['access_token'], data['account_spec'], data['account_id'], "Sell", symbol, 1, "Limit", True, order_price=float(trading_signal['tp1Line']))  # order qty = 1
+                    response_tp2 = place_order(data['access_token'], data['account_spec'], data['account_id'], "Sell", symbol, 1, "Limit", True, order_price=float(trading_signal['tp2Line']))  # order qty = 1
+                    response_tp3 = place_order(data['access_token'], data['account_spec'], data['account_id'], "Sell", symbol, 1, "Limit", True, order_price=float(trading_signal['tp3Line']))  # order qty = 1
+                    response_sl = place_order(data['access_token'], data['account_spec'], data['account_id'], "Sell", symbol, 3, "Stop", True, stopPrice=float(trading_signal['slLine']))  # order qty = 3
+                    response_sl_list.append(response_sl)
+                elif action_oco == 'Sell':
+                    response_entry = place_order(data['access_token'], data['account_spec'],data['account_id'], "Buy", symbol, 3, "Market", True)  # order qty = 3
+                    response_tp1 = place_order(data['access_token'], data['account_spec'], data['account_id'], "Sell", symbol, 1, "Limit", True, order_price=float(trading_signal['tp1Line']))  # order qty = 1
+                    response_tp2 = place_order(data['access_token'], data['account_spec'], data['account_id'], "Sell", symbol, 1, "Limit", True, order_price=float(trading_signal['tp2Line']))  # order qty = 1
+                    response_tp3 = place_order(data['access_token'], data['account_spec'], data['account_id'], "Sell", symbol, 1, "Limit", True, order_price=float(trading_signal['tp3Line']))  # order qty = 1
+                    response_sl = place_order(data['access_token'], data['account_spec'], data['account_id'], "Sell", symbol, 3, "Stop", True, stopPrice=float(trading_signal['slLine']))  # order qty = 3
+                    response_sl_list.append(response_sl)
+           
 
+        
+        elif action == "Tp1":
+            modify_sl1 = modify_order(data['access_token'], response_sl_list['orderId'], orderQty=2, orderType="Stop", stopPrice=float(trading_signal['slLine']))
+            # response_id.clear()
+            # response_id.append(response_sl)
 
+        elif action == "Tp2":
+            modify_sl2 = modify_order(data['access_token'], response_sl_list['orderId']['orderId'], orderQty=1, orderType="Stop", stopPrice=float(trading_signal['slLine']))
+            # response_id.clear()
+        
+        # elif action == "Sl Hit":
+        #     liquidation_resp = await liquidate_active_pos(accessToken)
 
-            # if order_type == "multiple_take_profit":
-                # response_entry = place_order(data['access_token'], ,data['account_spec'],data['account_id'], "Buy", symbol, 3, "Market", True)  # order qty = 3
-                # response_tp1 = place_order(accessToken, account_spec, account_id, "Sell", symbol, 1, "Limit", True, order_price=float(tp1))  # order qty = 1
-                # response_tp2 = place_order(accessToken, account_spec, account_id, "Sell", symbol, 1, "Limit", True, order_price=float(tp2))  # order qty = 1
-                # response_tp3 = place_order(accessToken, account_spec, account_id, "Sell", symbol, 1, "Limit", True, order_price=float(tp3))  # order qty = 1
-                # response_sl = place_order(accessToken, account_spec, account_id, "Sell", symbol, 3, "Stop", True, stopPrice=float(sl))  # order qty = 3
-            
             
             
             
